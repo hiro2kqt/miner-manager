6887c729c2fda3a9ebfb36a51e00b2dc9ef7c592
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 14:35:18 +0700

    update api

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
+const { default: axios } = require("axios");
 const express = require("express");
-const pm2 = require('pm2');
+const pm2 = require("pm2");
 const app = express();
 require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Arbius manager api");
 });
-app.get("/pm2/jobs", (req, res) => {
-  pm2.connect((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-    pm2.list((err, processes) => {
-      if (err) {
-        console.error(err);
-        pm2.disconnect();
-        res.status(500).json({ error: "Internal Server Error" });
-        return;
-      }
-      console.log(processes);
-      res.json({ processes });
-      pm2.disconnect();
-    });
-  });
+app.get("/jobs", async (req, res) => {
+  const listNode = JSON.parse(process.env.NODE_URL);
+  const status = await Promise.all(
+    listNode.map(async (obj) => {
+      const resp = await axios.get(`http://${obj}/pm2/jobs`);
+      return resp.data;
+    })
+  );
+  res.json(status);
 });
+app.get("./automine");
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at http://localhost:${process.env.PORT}`);
