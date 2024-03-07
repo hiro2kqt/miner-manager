5552b9dab0ad648f102d0796c6fb3d9e1ab5ef40
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 15:22:44 +0700

    update claim feature

--- index.js
+++ index.js
@@ -3,19 +3,32 @@ const express = require("express");
 const pm2 = require("pm2");
 const app = express();
 require("dotenv").config();
-
+const listNode = JSON.parse(process.env.NODE_URL);
 app.get("/", (req, res) => {
   res.send("Arbius manager api");
 });
 app.get("/jobs", async (req, res) => {
-  const listNode = JSON.parse(process.env.NODE_URL);
   const status = await Promise.all(
     listNode.map(async (obj) => {
-      const resp = await axios.get(`http://${obj}/pm2/jobs`);
+      const resp = await axios.get(`http://${obj?.url}/pm2/jobs`);
       return resp.data;
     })
   );
   res.json(status);
+});
+app.get("/claim", async (req, res) => {
+  try {
+    const newStatus = req?.query?.status;
+    const target = req?.query?.target;
+    const targetObj = listNode.find((e) => e?.name == target);
+    const resp = await axios.get(
+      `http://${targetObj?.url}/claim?status=${newStatus}`
+    );
+    res.json(resp.data);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
 });
 app.get("./automine");
 
