196b32cd032898ad3eed295ca052762977139cd5
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-10 17:23:38 +0700

    update autojob

--- automode.js
+++ automode.js
@@ -1,12 +1,8 @@
 const { default: axios } = require("axios");
 const { ethers } = require("ethers");
 const EngineABI = require("./V2_EngineV2.json");
-const { readFileSync, writeFileSync } = require("fs");
 const cron = require("node-cron");
 require("dotenv").config();
-const D_UP = 0;
-const D_DOWN = 1;
-const configPath = "config.json";
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const arbiusContract = new ethers.Contract(
   "0x3BF6050327Fa280Ee1B5F3e8Fd5EA2EfE8A6472a",
@@ -72,52 +68,33 @@ const fetchReward = async () => {
     return ethers.formatEther(reward);
   } catch (error) {}
 };
-const getCurrentConfig = async () => {
-  const data = readFileSync(configPath, "utf-8");
-  return JSON.parse(data);
-};
 const processAutomate = async () => {
   try {
+    const resp = await axios.get(`${process.env.CORE_URL}/autoclaim`);
+    const configf = resp.data;
+    if (!configf?.autoclaim?.enable) return;
+
     const gasFee = await fetchPrice();
     const reward = await fetchReward();
     const realReward = reward * 0.9;
-    console.log("current reward", realReward);
-    // const realReward = 0.0013;
-    let configf = await getCurrentConfig();
-    if (
-      realReward >= configf.autoclaim.thresshold &&
-      configf.autoclaim.direction == D_DOWN
-    ) {
-      console.log("run claim");
-      configf["autoclaim"]["direction"] = D_UP;
-      writeFileSync(configPath, JSON.stringify(configf));
+
+    if (realReward >= +configf.autoclaim.thresshold && !configf.autoclaim.on) {
+      console.log("run claim", realReward);
+      await axios.get(`${process.env.CORE_URL}/autoclaim/switch`);
     } else if (
-      realReward < configf.autoclaim.thresshold &&
-      configf.autoclaim.direction == D_UP
+      realReward <= +configf.autoclaim.thresshold &&
+      configf.autoclaim.on
     ) {
-      console.log("run stop claim");
-      configf["autoclaim"]["direction"] = D_DOWN;
-      writeFileSync(configPath, JSON.stringify(configf));
+      console.log("run stop claim", realReward);
+      await axios.get(`${process.env.CORE_URL}/autoclaim/switch`);
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-// (async () => {
-//   while (true) {
-//     try {
-//       await processAutomate();
-//       await delay(2000);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// })();
-
 const tenSecondlyTask = async () => {
-  // let configf = await getCurrentConfig();
-  // if (configf?.autoclaim?.enable) processAutomate();
+  processAutomate();
 };
 
 const cronExpression = "0 */10 * * * *";
